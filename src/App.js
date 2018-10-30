60d50ed73b36b55557bb7d30727958985537f93a
ssphyss <ss.physs@gmail.com>
2018-10-30 12:19:07 +0800

    branch1 完成檔案SASS串接&iconfont串接

--- src/App.js
+++ src/App.js
@@ -1,11 +1,102 @@
 import React, { Component } from 'react';
+import './sass/all.scss';
+import './assets/icon-font.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        啟動成功 11 Hello TODOList
-      </div>
+		<main className="wrapper">
+			<section className="section section-todo">
+			<div className="todo">
+				<h1 className="heading-primary">
+					<span className="heading-primary--main">Todo List</span>
+					<span className="heading-primary--sub">Built by Phy  for her CSS AND SASS. Copyright © by Phy.</span>
+				</h1>
+				<ul className="todo__list">
+				<li className="todo__item">
+					<div className="form__checkbox-group">
+						<input type="checkbox" className="form__checkbox-input" id="r1" />
+						<label for="r1" className="form__checkbox-label">
+							<span className="form__checkbox-button"></span>									         
+						</label>               
+					</div>
+					<div className="todo__content">
+						<div className="form__group">
+							<input className="form__input" placeholder="請輸入" type="text" />
+						</div>
+					</div>
+					<div className="todo__actions">
+						<a href='./'><i className="todo__icon icon-basic-todolist-pen"></i>編輯</a>
+						<a href='./'><i className="todo__icon icon-basic-trashcan"></i>刪除</a>
+					</div>
+				</li>
+				<li className="todo__item">
+					<div className="form__checkbox-group">
+						<input type="checkbox" className="form__checkbox-input" id="r2" />
+						<label for="r2" className="form__checkbox-label">
+						<span className="form__checkbox-button"></span>									         
+						</label>               
+					</div>
+					<div className="todo__content">
+						<div className="form__group">
+							<input className="form__input" placeholder="請輸入" type="text" />
+						</div>
+					</div>
+					<div className="todo__actions">
+						<a href='./'><i className="todo__icon icon-basic-todolist-pen"></i>編輯</a>
+						<a href='./'><i className="todo__icon icon-basic-trashcan"></i>刪除</a>
+					</div>
+				</li>
+				<li className="todo__item">
+					<div className="form__checkbox-group">
+						<input type="checkbox" className="form__checkbox-input" id="r3" />
+						<label for="r3" className="form__checkbox-label">
+							<span className="form__checkbox-button"></span>									         
+						</label>               
+					</div>
+					<div className="todo__content">
+						<div className="form__group">
+							<input className="form__input" placeholder="請輸入" type="text" />
+						</div>
+					</div>
+					<div className="todo__actions">
+						<a href='./'><i className="todo__icon icon-basic-todolist-pen"></i>編輯</a>
+						<a href='./'><i className="todo__icon icon-basic-trashcan"></i>刪除</a>
+					</div>
+				</li>
+				<li className="todo__item--complete">
+					<div className="form__checkbox-group">
+						<input type="checkbox" className="form__checkbox-input" id="r4" />
+						<label for="r4" className="form__checkbox-label">
+							<span className="form__checkbox-button"></span>									         
+						</label>               
+					</div>
+					<div className="todo__content">
+						<div className="form__group">
+							<input className="form__input" placeholder="請輸入" type="text" />
+						</div>
+					</div>
+					<div className="todo__actions">
+						<a href='./'><i className="todo__icon icon-basic-todolist-pen"></i>編輯</a>
+						<a href='./'><i className="todo__icon icon-basic-trashcan"></i>刪除</a>
+					</div>
+				</li>
+				<h5>新增任務</h5>	
+				<li className="todo__item--add">						
+					<div className="todo__content">
+						<div className="form__group">
+							<input className="form__input" placeholder="請輸入" type="text" />
+						</div>
+					</div>
+					<div className="todo__actions">
+						<a href='./'><i className="todo__icon icon-basic-todolist-pen"></i>送出</a>
+						<a href='./'><i className="todo__icon icon-basic-trashcan"></i>取消</a>
+					</div>
+				</li>
+				</ul>
+			</div>
+			</section>
+		</main>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import './style.js';                      // 引用全局js
-import './sass/all.scss';
+import './sass/all.scss'
 // import './assets/iconfont/iconfont.css';
 // import './assets/iconfont/iconfont.js';
 
